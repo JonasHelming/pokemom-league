48aeb6e58f2a4b9725c0e262e68cc69dc9f92c44
Jonas Helming <jhelming@eclipsesource.com>
2026-09-20 14:50:45 +0200

    Scaffold static site with Tailwind build

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ['./index.html', './src/**/*.js'],
+  theme: {
+    extend: {
+      fontFamily: { display: ['"Baloo 2"', 'cursive'] },
+      colors: {
+        typeFire: '#f87171',
+        typeWater: '#60a5fa',
+        typeGrass: '#4ade80',
+        typeElectric: '#facc15',
+      },
+    },
+  },
+  plugins: [],
+};
